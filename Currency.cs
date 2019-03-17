44393ab24d4d414ca8020735f40b163e08892c45
seanjasm <seanjasm@gmail.com>
2019-03-17 13:51:38 -0400

    My first prework at grand Circus- deliverable 1

---
+++ Currency.cs
@@ -0,0 +1,91 @@
+﻿using System;
+
+namespace Deliverable1
+{
+    enum currencyType { USD, Japanese, Thai, Swedish };
+
+    class NumberOperation
+    {
+        private double number1, number2, number3, sum;
+
+        public NumberOperation(double num1, double num2, double num3)
+        {
+            number1 = num1;
+            number2 = num2;
+            number3 = num3;
+            sum = num1 + num2 + num3;
+        }
+
+        public NumberOperation()
+        {
+            number1 = number2 = number3 = 0;
+        }
+
+        public string GetCurrencyByType(currencyType ctype)
+        {
+            switch (ctype)
+            {
+                case currencyType.Swedish:
+                    {
+                        return sum.ToString("C", System.Globalization.CultureInfo.CreateSpecificCulture("sv-SE"));
+                    }
+                case currencyType.Japanese:
+                    {
+                        return sum.ToString("C2", System.Globalization.CultureInfo.CreateSpecificCulture("ja-JP"));
+                    }
+                case currencyType.USD:
+                    {
+                        return sum.ToString("C2", System.Globalization.CultureInfo.CreateSpecificCulture("en-US"));
+                    }
+                case currencyType.Thai:
+                    {
+                        return sum.ToString("C", System.Globalization.CultureInfo.CreateSpecificCulture("th-TH"));
+                    }
+                default:
+                    return "";
+            }
+
+        }
+
+        public double GetSmallestNumber()
+        {
+            double smallest = number1;
+
+            if (number2 < smallest)
+            {
+                smallest = number2;
+
+                if (number3 < smallest)
+                    smallest = number3;
+
+            }
+            else if (number3 < smallest)
+                smallest = number3;
+
+            return smallest;
+        }
+
+        public double GetLargestNumber()
+        {
+            double largest = number1;
+
+            if (number2 > largest)
+            {
+                largest = number2;
+
+                if (number3 > largest)
+                    largest = number3;
+
+            }
+            else if (number3 > largest)
+                largest = number3;
+
+            return largest;
+        }
+
+        public double GetAverage()
+        {
+            return (number1 + number2 + number3)/3;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+
+namespace Deliverable1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double num1 = 0, num2 = 0, num3 = 0;
+            NumberOperation numberOperation = new NumberOperation();
+            
+
+            try
+            {
+                do
+                {
+                    Console.Clear();
+
+                    Console.Write("\nEnter first number then press ENTER: ");
+                    num1 = Convert.ToDouble(Console.ReadLine());
+
+                    Console.Write("\nEnter second number then press ENTER: ");
+                    num2 = Convert.ToDouble(Console.ReadLine());
+
+                    Console.Write("\nEnter third number then press ENTER: ");
+                    num3 = Convert.ToDouble(Console.ReadLine());
+
+                    numberOperation = new NumberOperation(num1, num2, num3);
+
+                    Console.WriteLine("\n\nHighest Number          Lowest Number          Average          ");
+
+                    Console.Write("\n\n{0, -24}", numberOperation.GetLargestNumber());
+                    Console.Write("{0, -23}", numberOperation.GetSmallestNumber());
+                    Console.Write("{0, -17}", numberOperation.GetAverage());
+
+
+
+                    Console.Write("\n\n\n  Total in Swedish Krona     {0}", numberOperation.GetCurrencyByType(currencyType.Swedish));
+                    Console.Write("\n\n   Total in Japanese Yen     {0}", numberOperation.GetCurrencyByType(currencyType.Japanese));
+                    Console.Write("\n\n      Total in US Dollar     {0}", numberOperation.GetCurrencyByType(currencyType.USD));
+                    Console.WriteLine("\n\n      Total in Thai Baht     {0}", numberOperation.GetCurrencyByType(currencyType.Thai));
+
+                    Console.Write("\n\n\nDo you want to continue?[N]:");
+
+                } while (Console.ReadLine().ToLower() == "y");
+
+            } catch
+            {
+                Console.WriteLine("\n\n\nThere was an error processing the info. Only accepts double/floating type numbers. \nPress ENTER to quit.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
